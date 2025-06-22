dcb5b2a43c65c24096c4a6dbed965f47c3d9089f
Chilamkurthi Bhuvansai <151933789+Bhuvansai-16@users.noreply.github.com>
2025-06-22 15:37:54 +0530

    Revert to original Flask version

--- app.py
+++ app.py
@@ -25,3 +25,6 @@ from routes import routes  # noqa: E402, F401
 
 # Register blueprints
 app.register_blueprint(routes)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
